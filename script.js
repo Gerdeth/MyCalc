538e1455d5c53bc78c97d28af08b6d56fc48fd4e
Gerdeth <gerdethwickham@gmail.com>
2021-10-09 11:45:48 -0500

    editeval()

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function display(val) {
     document.getElementById("textval").value+=val
 }
-function evaluate(){
-    let x = document.getElementById("textval").value
-    let y = eval(x)
-    document.getElementById("textval").value=y
- }
+function calculate()
+{
+ let x = document.getElementById("textval").value
+ let y = eval(x)
+ document.getElementById("textval").value = y
+ console.log(x)
+}
 
  function clr(){
      document.getElementById("textval").value =""
